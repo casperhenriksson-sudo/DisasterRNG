814389633a0998c542874e36b3650b7962043e2c
casperhenriksson-sudo <casper_henriksson@icloud.com>
2026-05-13 10:49:06 +0200

    Wire InventoryManager initialization into GameManager

--- src/server/GameManager.server.lua
+++ src/server/GameManager.server.lua
@@ -1,3 +1,4 @@
 local RoundManager = require(game.ServerScriptService.RoundManager)
+local InventoryManager = require(game.ServerScriptService.InventoryManager)
 RoundManager.Start()
 print("✅ GameManager startad!")
